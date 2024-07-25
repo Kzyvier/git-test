146e71a2709b75e9503c4dc13790d9da8384ad37
Xan <macvchase@gmail.com>
2024-07-25 19:56:34 +0900

    Java format

--- src/script.java
+++ src/script.java
@@ -1,6 +1,9 @@
 public class script {
     public static void main(String[] args) {
-        System.out.println("!!!!!!!!!!!!!!!!");
-        String s1 = new String();
+        System.out.printf("String %10d \n", 532);
+        System.out.printf("String %10d \n", 5);
+        System.out.printf("String %10d \n", 100000);
+        System.out.printf("String %10d \n", 1000000000);
+
     }
 }
