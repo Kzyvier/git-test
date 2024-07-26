871c468336b6a8232b65a398e6a97b83674c0156
Xan <macvchase@gmail.com>
2024-07-26 22:34:24 +0900

    Interfaces

---
+++ src/Interfaces/Animal.java
@@ -0,0 +1,16 @@
+package Interfaces;
+
+public class Animal {
+    public int id;
+
+    public Animal(int id){
+        this.id = id;
+
+    }
+
+    public void sleep(){
+        System.out.println("Im sleeping");
+
+
+    }
+}
---
+++ src/Interfaces/Test.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public class Test {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal(1);
+        Person person1 = new Person("Matt");
+        animal1.sleep();
+        person1.sayHello();
+
+    }
+}
